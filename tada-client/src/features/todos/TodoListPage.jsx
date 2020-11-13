3aeed983c76895d4ddbf739db5c32064464ab5b4
tattsun <tattsun513@gmail.com>
2020-11-13 23:20:45 +0900

    fix

--- tada-client/src/features/todos/TodoListPage.jsx
+++ tada-client/src/features/todos/TodoListPage.jsx
@@ -1,22 +1,35 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { selectAllTodos } from './todosSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectAllTodos, todoSetDoneStatus } from './todosSlice';
 import classes from './todos.module.css';
 import TodoInputForm from './TodoInputForm';
 
 const TodoListPage = () => {
   const todos = useSelector(selectAllTodos);
 
+  const dispatch = useDispatch();
+
   const orderedTodos = todos.slice().sort((a, b) => b.order - a.order);
+
+  const onDoneClicked = (todoId, done) => () => {
+    dispatch(todoSetDoneStatus({ id: todoId, done }));
+  };
 
   const todoItems = orderedTodos.map((todo) => (
     <div className={classes.TodoItem} key={todo.id}>
-      {todo.text}
-      {' '}
-      <span className={classes.TodoId}>
-        (
-        {todo.id}
-        )
+      <input
+        type="checkbox"
+        checked={todo.done}
+        onChange={onDoneClicked(todo.id, !todo.done)}
+      />
+      <span className={todo.done ? classes.TodoTextDone : ''}>
+        {todo.text}
+        {' '}
+        <span className={classes.TodoId}>
+          (
+          {todo.id}
+          )
+        </span>
       </span>
     </div>
   ));
--- tada-client/src/features/todos/todosSlice.js
+++ tada-client/src/features/todos/todosSlice.js
@@ -2,8 +2,12 @@ import { createSlice, nanoid } from '@reduxjs/toolkit';
 
 const initialState = {
   todos: [
-    { id: nanoid(), text: 'Hello, world', order: 12345 },
-    { id: nanoid(), text: 'Foobar', order: 11 },
+    {
+      id: nanoid(), text: 'Hello, world', order: 12345, done: false,
+    },
+    {
+      id: nanoid(), text: 'Foobar', order: 11, done: false,
+    },
   ],
 };
 
@@ -24,14 +28,25 @@ const todosSlice = createSlice({
         id: nanoid(),
         text,
         order: maxOrder + 1000,
+        done: false,
       };
 
       state.todos.push(todo);
     },
+    todoSetDoneStatus(state, action) {
+      const { id, done } = action.payload;
+
+      state.todos.forEach((todo) => {
+        if (todo.id === id) {
+          // eslint-disable-next-line no-param-reassign
+          todo.done = done;
+        }
+      });
+    },
   },
 });
 
-export const { todoAdded } = todosSlice.actions;
+export const { todoAdded, todoSetDoneStatus } = todosSlice.actions;
 
 export default todosSlice.reducer;
 
